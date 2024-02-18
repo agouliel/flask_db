140c6ce7b4080b446e5cf68f3b47927a91956d95
agouliel <agouliel@gmail.com>
2024-02-18 12:27:49 +0200

    added cors

--- app_pkg/__init__.py
+++ app_pkg/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_cors import CORS
 
 db = SQLAlchemy()
 login = LoginManager()
@@ -9,6 +10,7 @@ login = LoginManager()
 def create_app(config_class=Config):
   app = Flask(__name__)
   app.config.from_object(config_class)
+  CORS(app)
   db.init_app(app)
   login.init_app(app)
 
